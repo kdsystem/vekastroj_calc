2c71c120ba9df640e39121c0039f31e00eb6067c
kdsystem <kdsystem@gmail.com>
2017-05-25 12:46:29 +0300

    4

--- html/check_prom.php
+++ html/check_prom.php
@@ -1,5 +1,6 @@
 <?php
 $debugging=false;
+//ну тут всё ясно
 function csv_in_array($url, $delm = ";", $encl = "\"", $head = false, $wid, $heig, $type) {
 	$csvxrow = file ( $url );
 	$csvxrow [0] = chop ( $csvxrow [0] );
