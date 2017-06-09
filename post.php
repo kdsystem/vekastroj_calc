6fdb0b292782caa910921240547bdda1a6d8c6cc
kdsystem <kdsystem@gmail.com>
2017-06-09 13:16:32 +0300

    qw

--- post.php
+++ post.php
@@ -26,7 +26,11 @@ else
 	
 	// Отправляем письмо админу
 	
-	mail ( "$adminemail", "$date, $time, Сообщение от $name", "$msg" );
+	$headers  = "Content-type: text/html; charset=windows-1251 \r\n";
+	$headers .= "From: От кого письмо <from@example.com>\r\n";
+	$headers .= "Reply-To: reply-to@example.com\r\n"; 
+	
+	mail ( $adminemail, "$date $time Сообщение от $name", $msg, $headers );
 	
 	// Сохраняем в базу данных
 	
