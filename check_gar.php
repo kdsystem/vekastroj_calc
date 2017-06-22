7d6e3c5af4f130fe2186b6dec58140ba789abb41
kdsystem <kdsystem@gmail.com>
2017-06-22 22:55:33 +0300

    1

--- check_gar.php
+++ check_gar.php
@@ -154,15 +154,15 @@ if ($montazh != "none") {
 
 if ($upr == "automatic") {
 	if (($csvdata [2]*$csvdata [3] <= 8400000) & ($csvdata [3]<=2700))  {
-		$price_automatic=20250;
+		$price_automatic=7350;
 		$price_automatic_text="ASG600/3KIT-L Электропривод со встроенным блоком управления, встроенный радиоприемник, два четырехканальных пульта, рейка с цепью 3,5м., 24В, тяговое усилие 600Н";
 	}
 	if (($csvdata [2]*$csvdata [3] <= 13500000) & ($csvdata [2]*$csvdata [3] > 8400000) & ($csvdata [3]<=2700)){
-		$price_automatic=28100;
+		$price_automatic=8200;
 		$price_automatic_text="ASG1000/3KIT-L Электропривод со встроенным блоком управления, встроенный радиоприемник, два четырехканальных пульта, рейка с цепью 3,5м., 24В, тяговое усилие 1000Н";
 	}
 	if (($csvdata [2]*$csvdata [3] <= 16000000) & ($csvdata [2]*$csvdata [3] > 13500000) & ($csvdata [3]>2700) & ($csvdata [3]<=3400)){
-		$price_automatic=28100;
+		$price_automatic=9450;
 		$price_automatic_text="ASG1000/4KIT Электропривод со встроенным блоком управления, встроенный радиоприемник, два четырехканальных пульта, рейка с цепью 4,2м., 24В, тяговое усилие 1000Н";
 	}
 }
@@ -365,6 +365,31 @@ if ($door=="door_low") {
 	echo '</td>';
 	echo '</tr>';
 }
+
+if ($upr == "automatic") {
+	echo '<tr>';
+	echo '<td>';
+	echo $price_automatic_text;
+	echo '</td>';
+	echo '<td>';
+	echo $price_automatic." руб.";
+	echo '</td>';
+	if ($door !="none"){
+		$price_automatic_dop=2850;
+		$price_automatic_dop_dicount=$price_automatic_dop*(1-$discount);
+		echo '<tr>';
+		echo '<td>';
+		echo "Коммутационный набор для подключения к системам управления электроприводами датчиков безопасности";
+		echo "(датчика калитки и датчиков ослабления тяговых тросов)";
+		echo '</td>';
+		echo '<td>';
+		echo $price_automatic_dop." руб.";
+		echo '</td>';
+		echo '<td>';
+		echo $price_automatic_dop_dicount." руб.";
+		echo '</td>';
+	}
+	echo '</tr>';
 
 
 if ($dostavka<>"none"){
