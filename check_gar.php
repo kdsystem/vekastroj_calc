84553336f38d4b7c6b41a4ace15b15cfd86489b2
kdsystem <kdsystem@gmail.com>
2017-06-22 21:37:35 +0300

    1

--- check_gar.php
+++ check_gar.php
@@ -294,7 +294,7 @@ echo 'Сумма';
 echo '</tr>';
 echo '<tr>';
 echo '<td>';
-echo 'Гаражные ворота ';
+echo 'Гаражные ворота1 ';
 echo "ширина = " . $csvdata [2]."мм., высота = " . $csvdata [3]."мм.".'(профиль '.$maintype.', полотно '.$poltype.', ';
 echo 'цвет ='.$selected_color.",";
 if ($mount_type=="std_mount") { echo '<br>';echo 'выбран стандартный тип монтажа';}
