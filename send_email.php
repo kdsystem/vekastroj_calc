7b9d918a11286cf004a0b5fe5b62d223817744eb
kdsystem <kdsystem@gmail.com>
2017-06-26 16:19:15 +0300

    aaa

--- send_email.php
+++ send_email.php
@@ -1,10 +1,27 @@
 <?php 
 	$to      = 'kdsystem@gmail.com';
 	$subject = 'Заказ с сайта';
-	$text=serialize($_POST);
 	$headers = 'From: webmaster@wss.spb.ru' . "\r\n" .
 			'Reply-To: lubimov@wss.spb.ru' . "\r\n" .
 			'X-Mailer: PHP/' . phpversion();
 
-	mail($to, $subject, $text, $headers); 	
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	
+	// Дополнительные заголовки
+	$headers[] = 'To: Alex Lubimov <lubimov@wss.spb.ru>';
+	$headers[] = 'From: Webmaster <admin@wss.spb.ru>';
+
+	$message = '
+		<html>
+			<head>
+  				<title>Заказ с сайта</title>
+			</head>
+			<body>';
+	$text=$message.serialize($_POST).'</body>
+	</html>
+	';
+	
+	
+	mail($to, $subject, $text, implode("\r\n", $headers)); 	
 ?>
